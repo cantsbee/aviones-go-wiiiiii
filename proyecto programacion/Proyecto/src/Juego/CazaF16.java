e7ad8c4ddb8083834947e99d5c90e46dc04c3d13
cantsbee <juanita.zuluagaacosta@gmail.com>
2025-02-14 12:24:20 +0100

    detalles avion 16

--- proyecto programacion/Proyecto/src/Juego/CazaF16.java
+++ proyecto programacion/Proyecto/src/Juego/CazaF16.java
@@ -2,4 +2,11 @@ package Juego;
 
 public class CazaF16 extends Avion {
 
+    Double danio;
+    Double defensa;
+    Double velocidad;
+    String herramienta;
+    
+
+
 }
--- proyecto programacion/Proyecto/src/Juego/combate.java
+++ proyecto programacion/Proyecto/src/Juego/combate.java
@@ -1,5 +1,6 @@
 package Juego;
 
 public class combate {
-	
+	String ataque;
+    String defensa;
 }
