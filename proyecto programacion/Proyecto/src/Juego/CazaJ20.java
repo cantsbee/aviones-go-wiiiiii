46c886a5ab873ab6a0fdc78d1a1fdcb6019e70cd
Johan_Villamarin <johan17011@outlook.com>
2025-02-14 12:32:29 +0100

    Constructores de aviones

--- proyecto programacion/Proyecto/src/Juego/CazaJ20.java
+++ proyecto programacion/Proyecto/src/Juego/CazaJ20.java
@@ -1,5 +1,21 @@
 package Juego;
 
-public class CazaJ20 extends Avion {
-    
+public class CazaJ20 extends Avion{
+	double danio;
+	double defensa;
+	double velocidad;
+	String herramienta;
+	
+	public CazaJ20(double peso, String nombre, String modelo, double velocidad, double danio, String tipoAtaque,
+			String opcion, double danio2, double defensa, double velocidad2, String herramienta) {
+		
+		super(peso, nombre, modelo, velocidad, danio, tipoAtaque, opcion);
+		
+		danio = danio2;
+		this.defensa = defensa;
+		velocidad = velocidad2;
+		this.herramienta = herramienta;
+	}
+	
+	
 }
