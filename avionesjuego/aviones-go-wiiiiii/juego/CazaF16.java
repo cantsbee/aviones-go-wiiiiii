899bfbd25c06d720f032388957ef334b9897f38a
Juanita Zuluaga Acosta <59172512+cantsbee@users.noreply.github.com>
2025-02-21 14:29:05 +0100

    Update CazaF16.java

--- avionesjuego/aviones-go-wiiiiii/juego/CazaF16.java
+++ avionesjuego/aviones-go-wiiiiii/juego/CazaF16.java
@@ -62,4 +62,7 @@ public class CazaF16 extends Avion {
         activarHabilidad=true;
         System.out.println(nombre + " activa Velocidad Supersónica, duplica su próximo ataque!");
     }
+
+    public void mostrarDatos(){
+            System.out.println("Las características de este caza son: \n daño = {10, 20, 35}; \n velocidad= 900 \n maniobrabilidad= 85 \n vida=350");
 }
