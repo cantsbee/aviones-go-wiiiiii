710dba1f21017f60256fb1846828f4ccb8698c66
Johanvillamarin <johan17011@outlook.com>
2025-02-21 14:45:52 +0100

    Update AppJuego.java

--- avionesjuego/aviones-go-wiiiiii/juego/AppJuego.java
+++ avionesjuego/aviones-go-wiiiiii/juego/AppJuego.java
@@ -4,6 +4,11 @@ import java.util.*;
 public class AppJuego { 
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
+        System.out.println("\n*********************************************\n              BIENVENIDO AL JUEGO!        \n*********************************************\n");
+        System.out.println("Selecciona 1 para iniciar batalla, 2 para mostrar stats de cada personaje. Elige con cuidado, soldado!")
+        int elegir=lector.nextInt();
+        if (elegir==1) {
+        System.out.println("\nJugador 1, elige tu personaje:\n'1' Caza F16 \n'2' Caza F35 \n'3' Caza J20 \n'4' Portaaviones Clase Nimitz");
         System.out.println("Selecciona 1 para iniciar batalla, 2 para mostrar stats de cada personaje. Elige con cuidado, soldado!")
         int elegir=lector.nextInt();
         if (elegir==1) {
@@ -13,14 +18,14 @@ public class AppJuego {
         
         System.out.println("Jugador 1, elige tu personaje:\n '1' Caza F16 \n'2' Caza F35 \n'3' Caza J20 \n'4' Portaaviones Clase Nimitz");
         int eleccion1 = OpcionValida(lector);
-        System.out.println("Ingresa el nombre de tu aeronave: ");
+        System.out.println("\nIngresa el nombre de tu aeronave: ");
         lector.nextLine();
         String nombre1 = lector.nextLine();
         Personaje jugador1 = crearPersonaje(eleccion1, nombre1);
 
-        System.out.println("Jugador 2, elige tu personaje: '1' Caza F16 \n'2' Caza F35 \n'3' Caza J20 \n'4' Portaaviones Clase Nimitz");
+        System.out.println("\nJugador 2, elige tu personaje:\n'1' Caza F16 \n'2' Caza F35 \n'3' Caza J20 \n'4' Portaaviones Clase Nimitz");
         int eleccion2 = OpcionValida(lector);
-        System.out.println("Ingresa el nombre de tu aeronave: ");
+        System.out.println("\nIngresa el nombre de tu aeronave: ");
         lector.nextLine();
         String nombre2 = lector.nextLine();
         Personaje jugador2 = crearPersonaje(eleccion2, nombre2);
@@ -33,11 +38,21 @@ public class AppJuego {
 
         System.out.println("\nJuego terminado.");
         lector.close();
+    }else if(elegir==2){
+        System.out.println("esc");
+        int opcionn=lector.nextInt();
+        if(opcionn==1){
+            CazaF16.Mostradatos
+         }else if(){
+
+         }
+        
     }
+}
 
     public static void ejecutarTurno(Personaje atacante, Personaje defensor, Scanner lector) {
         System.out.println("\nTurno de " + atacante.getNombre());
-        System.out.println("¿Qué deseas hacer? '1' Atacar  '2' Activar Habilidad");
+        System.out.println("¿Qué deseas hacer?\n'1' Atacar  \n'2' Activar Habilidad");
         int accion = lector.nextInt();
         if (accion == 1) {
             atacante.atacar(defensor);
@@ -50,25 +65,28 @@ public class AppJuego {
 public static int OpcionValida(Scanner lector) {
     int opcion = 0;
     while (true) {
-        System.out.println("Ingresa tu elección: ");
+        System.out.println("\nIngresa tu elección: ");
         try {
             opcion = lector.nextInt();
             if (opcion >= 1 && opcion <= 4) {
                 return opcion;
             } else {
-                System.out.println("Número fuera de rango, intenta de nuevo.");
+                System.out.println("\nNúmero fuera de rango, intenta de nuevo.");
             }
         } catch (Exception e) {
-            System.out.println("Entrada no válida, intenta de nuevo.");
+            System.out.println("\nEntrada no válida, intenta de nuevo.");
             lector.nextLine();  
         }
     }
 }
 
+
+
+
     public static Personaje crearPersonaje(int eleccion, String nombre) {
         switch (eleccion) {
             case 1:
-            System.out.println("Has elegido un caza F16");
+            System.out.println("\nHas elegido un caza F16\n");
             System.out.println("                __|__");
             System.out.println("       ---o-----(_)--(_)-----o---");
             System.out.println("            //             /");
@@ -81,7 +99,7 @@ public static int OpcionValida(Scanner lector) {
             System.out.println("           |__|_|_|_|__|");;
             return new CazaF16(nombre);
             case 2:
-            System.out.println("Has elegido un Caza F35");
+            System.out.println("\nHas elegido un Caza F35\n");
             System.out.println("             __|__");
             System.out.println("      ------[==o==]------");
             System.out.println("            /  |  //");
@@ -93,7 +111,7 @@ public static int OpcionValida(Scanner lector) {
             return new CazaF35(nombre);
 
             case 3: 
-            System.out.println("Has elegidp un caza J20");
+            System.out.println("\nHas elegidp un caza J20\n");
             System.out.println("                ///");
             System.out.println("         ______/  //______");
             System.out.println("  -------|   [====]   |-------");
@@ -105,7 +123,7 @@ public static int OpcionValida(Scanner lector) {
             return new CazaJ20(nombre);
 
             case 4: 
-            System.out.println("Has elegido un portaaviones clase Nimitz");
+            System.out.println("\nHas elegido un portaaviones clase Nimitz\n");
             System.out.println("         ___________________________________________________");
             System.out.println("        |                                                   |");
             System.out.println("        |             AIRCRAFT CARRIER                      |");
@@ -115,7 +133,7 @@ public static int OpcionValida(Scanner lector) {
 
             return new PortaavionesClaseNimitz(nombre);
             default:
-                System.out.println("Opción incorrecta, se te asignara un avion por defecto");
+                System.out.println("\nOpción incorrecta, se te asignara un avion por defecto");
                 return new CazaJ20(nombre);
         }
     }
