d76b0d832f518a58d8100807afa8bada7a664ec0
Johanvillamarin <johan17011@outlook.com>
2025-02-26 10:05:38 +0100

    Add files via upload

---
+++ proyecto final programacion/juego/PortaavionesClaseNimitz.java
@@ -0,0 +1,30 @@
+package juego;
+
+public class PortaavionesClaseNimitz extends Portaaviones {
+    public PortaavionesClaseNimitz(String nombre) {
+        super(nombre, 120, 350, 600, "Misiles", 300, 620);
+    }
+    
+    @Override
+    public void atacar(Personaje enemigo) {
+        int[] danos = {10, 17, 15};
+        int danio = danos[random.nextInt(danos.length)];
+        String[] sonidos = {"¡Kaboom!", "¡Boom!", "¡Booom!"};
+        System.out.println(sonidos[random.nextInt(sonidos.length)]);
+        enemigo.recibirDanio(danio,this);
+    }
+    @Override
+    public int getVida(){
+        return vida;
+    }
+    @Override
+    public void activarHabilidad() {
+        esquivar=true;
+        System.out.println(nombre + " tiene la probabilidad de esquivar el proximo ataque enemigo");
+    }
+    public static void mostrarDatos(){
+        System.out.println("Las características de este portaaviones son: \n daño = {10, 17, 15};\n capacidad de aviones: 120 aviones \n blindaje: 350 puntos \n vida=600 puntos\n usa un sistema de defensa con torpedos\ntiene una velocidad de 300 km/h\n y cuenta con un poder de 620 ");
+    }
+    
+}
+
---
+++ proyecto final programacion/juego/PortaavionesClaseQueenElizabeth.java
@@ -0,0 +1,29 @@
+package juego;
+
+public class PortaavionesClaseQueenElizabeth extends Portaaviones {
+    public PortaavionesClaseQueenElizabeth(String nombre) {
+        super(nombre, 120, 500, 650, "Torpedos", 450, 550);
+    }
+    
+    @Override
+    public void atacar(Personaje enemigo) {
+        int[] danos = {11, 14, 16};
+        int danio = danos[random.nextInt(danos.length)];
+        String[] sonidos = {"¡Kaboom!", "¡Boom!", "¡Booom!"};
+        System.out.println(sonidos[random.nextInt(sonidos.length)]);
+        enemigo.recibirDanio(danio,this);
+    }
+    @Override
+    public int getVida(){
+        return vida;
+    }
+    @Override
+    public void activarHabilidad() {
+        contraataque=true;
+        System.out.println(nombre + " Contraataca al recibir daño");
+    }
+    public static void mostrarDatos(){
+        System.out.println("Las características de este portaaviones son: \n daño = {11, 14, 16};\n capacidad de aviones: 120 \n blindaje: 500 puntos \n vida=650 puntos\n usa un sistema de defensa con lanza cohetes\ntiene una velocidad de 450 km/h\n y cuenta con un poder de 550 ");
+    }
+    
+}
